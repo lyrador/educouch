8571b24cf90ddb949ac0d624a25620660e9f2849
unknown <irenelie1412@gmail.com>
2022-08-29 23:31:56 +0800

    trying to add educator class

---
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/model/Educator.java
@@ -0,0 +1,68 @@
+package com.educouch.educouchsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Educator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long educatorId;
+    private String name;
+    private String email;
+    private String password;
+    private String username;
+    private String profilePicture;
+
+    public Long getEducatorId() {
+        return educatorId;
+    }
+
+    public void setEducatorId(Long educatorId) {
+        this.educatorId = educatorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
