31310d31f208f5e6c4725155905171b4b4e13cfb
lyrador <yeedaryl700@gmail.com>
2022-09-11 12:20:02 +0800

    added in postgres support + commandlinerunner

--- educouchsystem/src/main/java/com/educouch/educouchsystem/EduCouchSystemApplication.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/EduCouchSystemApplication.java
@@ -1,13 +1,25 @@
 package com.educouch.educouchsystem;
 
+import com.educouch.educouchsystem.model.Learner;
+import com.educouch.educouchsystem.repository.LearnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 	@SpringBootApplication
-	public class EduCouchSystemApplication {
+	public class EduCouchSystemApplication implements CommandLineRunner {
+
+	@Autowired
+	private LearnerRepository learnerRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EduCouchSystemApplication.class, args);
 	}
 
-}
+		//initialise new entities here
+		@Override
+		public void run(String... args) throws Exception {
+			learnerRepository.save(new Learner("Alex", "SG", "alex@gmail.com", "password", "alex", "https://educouchbucket.s3.ap-southeast-1.amazonaws.com/1662869709706_alex.png"));
+		}
+	}
