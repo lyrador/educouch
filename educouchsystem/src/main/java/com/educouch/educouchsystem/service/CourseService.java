cf976b6313d6e900675016529723fdd003e33e18
sachok123 <e0540367@u.nus.edu>
2022-09-13 21:46:09 +0800

    added full CRUD to course

--- educouchsystem/src/main/java/com/educouch/educouchsystem/service/CourseService.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/service/CourseService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface CourseService {
     public Course saveCourse(Course course);
     public List<Course> getAllCourses();
+    public Course retrieveCourseById(Long id);
+    public void deleteCourse(Long id);
 }
