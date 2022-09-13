743ce05af560eeb5ccdd508662f044f78646e7ce
sachok123 <e0540367@u.nus.edu>
2022-09-13 21:46:09 +0800

    added full CRUD to course

--- educouchsystem/src/main/java/com/educouch/educouchsystem/controller/CourseController.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/controller/CourseController.java
@@ -3,9 +3,12 @@ package com.educouch.educouchsystem.controller;
 import com.educouch.educouchsystem.model.Course;
 import com.educouch.educouchsystem.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/course")
@@ -23,4 +26,35 @@ public class CourseController {
     public List<Course> getAllCourses() {
         return courseService.getAllCourses();
     }
+
+    @GetMapping("/course/{courseId}")
+    public ResponseEntity<Course> retrieveCourseById(@PathVariable("courseId") Long courseId) {
+        try {
+            Course existingCourse = courseService.retrieveCourseById(courseId);
+            return new ResponseEntity<Course>(existingCourse, HttpStatus.OK);
+        } catch (NoSuchElementException ex) {
+            return new ResponseEntity<Course>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/course/{courseId}")
+    public String deleteCourse(@PathVariable("courseId") Long courseId) {
+        courseService.deleteCourse(courseId);
+        return "Deleted Course with id " + courseId; 
+    }
+
+    @PutMapping("/course/{courseId}")
+    public ResponseEntity<Course> updateCourse(@RequestBody Course course, @PathVariable Long courseId) {
+        try {
+            Course existingCourse = courseService.retrieveCourseById(courseId);
+
+            Course updatedCourse = courseService.saveCourse(course);
+            return new ResponseEntity<Course>(updatedCourse, HttpStatus.OK);
+        } catch (NoSuchElementException ex) {
+            return new ResponseEntity<Course>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+
 }
