7b73c859c53c7533f15f4c1633eb0d575bd402b1
SrinivasG99 <78495960+SrinivasG99@users.noreply.github.com>
2022-09-15 12:40:55 +0800

    basic exception handling added

---
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/exception/LmsAdminNotFoundException.java
@@ -0,0 +1,11 @@
+package com.educouch.educouchsystem.exception;
+
+public class LmsAdminNotFoundException extends RuntimeException{
+
+
+    public LmsAdminNotFoundException() {
+    }
+    public LmsAdminNotFoundException(String message) {
+        super(message);
+    }
+}
