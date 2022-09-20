93cdd0266b7d584c8edf5f3fd7db0d2f9b4e5253
ychenxuan123 <yuan.chenxuan@u.nus.edu>
2022-09-20 21:31:59 +0800

    Enum

--- educouchsystem/src/main/java/com/educouch/educouchsystem/util/enumeration/AssessmentStatusEnum.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/util/enumeration/AssessmentStatusEnum.java
@@ -1,18 +1,5 @@
 package com.educouch.educouchsystem.util.enumeration;
 
 public enum AssessmentStatusEnum {
-    PENDING("Pending"),
-    INCOMPLETE("Incomplete"),
-    COMPLETE("Complete");
-
-    private final String name;
-
-    private AssessmentStatusEnum(String str) {
-        name = str;
-    }
-
-    public String toString() {
-        return this.name;
-    }
-
+    PENDING, INCOMPLETE, COMPLETE
 }
--- educouchsystem/src/main/java/com/educouch/educouchsystem/util/enumeration/QuestionTypeEnum.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/util/enumeration/QuestionTypeEnum.java
@@ -2,19 +2,6 @@ package com.educouch.educouchsystem.util.enumeration;
 
 public enum QuestionTypeEnum {
 
-    TRUE_FALSE("TrueFalse"),
-    MCQ("MultipleChoice"),
-    MRQ("MultipleResponse"),
-    OPEN_ENDED("OpenEnded");
-
-    private final String name;
-
-    private QuestionTypeEnum(String str) {
-        name = str;
-    }
-
-    public String toString() {
-        return this.name;
-    }
+    TRUE_FALSE, MCQ, MRQ, OPEN_ENDED
 
 }
