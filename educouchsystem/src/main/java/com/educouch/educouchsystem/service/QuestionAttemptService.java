7276bd9779e4ef753918995b04af3ed0305f6f9a
ychenxuan123 <yuan.chenxuan@u.nus.edu>
2022-09-22 18:11:04 +0800

    Further changes to BE and logic

---
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/service/QuestionAttemptService.java
@@ -0,0 +1,24 @@
+package com.educouch.educouchsystem.service;
+
+import com.educouch.educouchsystem.model.Option;
+import com.educouch.educouchsystem.model.QuestionAttempt;
+import com.educouch.educouchsystem.util.exception.QuestionAttemptNotFoundException;
+import com.educouch.educouchsystem.util.exception.QuestionNotFoundException;
+
+import java.util.List;
+
+public interface QuestionAttemptService {
+
+    public QuestionAttempt saveQuestionAttempt(QuestionAttempt questionAttempt);
+
+    public List<QuestionAttempt> getAllQuestionAttempts();
+
+    public List<Option> getSubmittedOptions(Long questionAttemptId) throws QuestionAttemptNotFoundException;
+
+    public QuestionAttempt retrieveQuestionAttemptById(Long questionAttemptId) throws QuestionAttemptNotFoundException;
+
+    public void deleteQuestionAttempt(Long questionAttemptId) throws QuestionAttemptNotFoundException;
+
+    public void calculateQuestionScore(Long questionId, Long questionAttemptId) throws QuestionAttemptNotFoundException, QuestionNotFoundException;
+
+}
