4cfff1d29643bce67033bf0747b6a31302ff1d5b
ychenxuan123 <yuan.chenxuan@u.nus.edu>
2022-09-25 18:42:46 +0800

    From Main

--- educouchsystem/src/main/java/com/educouch/educouchsystem/controller/AssessmentController.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/controller/AssessmentController.java
@@ -1,5 +1,6 @@
 package com.educouch.educouchsystem.controller;
 
+import com.educouch.educouchsystem.dto.FileSubmissionDTO;
 import com.educouch.educouchsystem.model.Assessment;
 import com.educouch.educouchsystem.model.Course;
 import com.educouch.educouchsystem.model.FileSubmission;
@@ -8,6 +9,8 @@ import com.educouch.educouchsystem.service.AssessmentService;
 import com.educouch.educouchsystem.service.CourseService;
 import com.educouch.educouchsystem.service.FileSubmissionService;
 import com.educouch.educouchsystem.service.QuizService;
+import com.educouch.educouchsystem.util.enumeration.AssessmentStatusEnum;
+import com.educouch.educouchsystem.util.enumeration.FileSubmissionEnum;
 import com.educouch.educouchsystem.util.exception.AssessmentNotFoundException;
 import com.educouch.educouchsystem.util.exception.CourseNotFoundException;
 import com.educouch.educouchsystem.util.exception.FileSubmissionNotFoundException;
@@ -17,7 +20,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -48,21 +57,66 @@ public class AssessmentController {
 //    }
 
     @PostMapping("/addNewFileSubmission/{courseId}")
-    public ResponseEntity<FileSubmission> addFileSubmission(@RequestBody FileSubmission fileSubmission, @PathVariable(value="courseId") Long courseId) {
+    public ResponseEntity<FileSubmission> addFileSubmission(@RequestBody FileSubmissionDTO fileSubmissionDTO, @PathVariable(value="courseId") Long courseId) {
         try {
             Course course = courseService.retrieveCourseById(courseId);
-            fileSubmissionService.saveFileSubmission(courseId, fileSubmission);
-            return new ResponseEntity<>(fileSubmission, HttpStatus.OK);
-        } catch (CourseNotFoundException ex) {
+            FileSubmission newFileSubmission = new FileSubmission();
+            newFileSubmission.setTitle(fileSubmissionDTO.getAssessmentTitle());
+            newFileSubmission.setDescription(fileSubmissionDTO.getAssessmentDescription());
+            newFileSubmission.setMaxScore(fileSubmissionDTO.getAssessmentMaxScore());
+
+            if (fileSubmissionDTO.getAssessmentFileSubmissionEnum().equals("INDIVIDUAL")) {
+                newFileSubmission.setFileSubmissionEnum(FileSubmissionEnum.INDIVIDUAL);
+            } else if (fileSubmissionDTO.getAssessmentFileSubmissionEnum().equals("GROUP")) {
+                newFileSubmission.setFileSubmissionEnum(FileSubmissionEnum.GROUP);
+            }
+
+            if (fileSubmissionDTO.getAssessmentIsOpen().equals("true")) {
+                newFileSubmission.setOpen(Boolean.TRUE);
+            } else if (fileSubmissionDTO.getAssessmentIsOpen().equals("false")) {
+                newFileSubmission.setOpen(Boolean.FALSE);
+            }
+
+            if (fileSubmissionDTO.getAssessmentStatusEnum().equals("PENDING")) {
+                newFileSubmission.setAssessmentStatus(AssessmentStatusEnum.PENDING);
+            } else if (fileSubmissionDTO.getAssessmentStatusEnum().equals("INCOMPLETE")) {
+                newFileSubmission.setAssessmentStatus(AssessmentStatusEnum.INCOMPLETE);
+            } else if (fileSubmissionDTO.getAssessmentStatusEnum().equals("COMPLETE")) {
+                newFileSubmission.setAssessmentStatus(AssessmentStatusEnum.COMPLETE);
+            }
+
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date startDate = (Date)formatter.parse(fileSubmissionDTO.getAssessmentStartDate());
+            Date endDate = (Date)formatter.parse(fileSubmissionDTO.getAssessmentEndDate());
+            newFileSubmission.setStartDate(startDate);
+            newFileSubmission.setEndDate(endDate);
+
+            fileSubmissionService.saveFileSubmission(courseId, newFileSubmission);
+            return new ResponseEntity<>(newFileSubmission, HttpStatus.OK);
+        } catch (CourseNotFoundException | ParseException ex) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/getAllFileSubmissionsByCourseId/{courseId}")
-    public ResponseEntity<List<FileSubmission>> getAllFileSubmissionsByCourseId (@PathVariable(value="courseId") Long courseId) {
+    public ResponseEntity<List<FileSubmissionDTO>> getAllFileSubmissionsByCourseId (@PathVariable(value="courseId") Long courseId) {
         try {
             List<FileSubmission> fileSubmissions = fileSubmissionService.getAllFileSubmissionByCourseId(courseId);
-            return new ResponseEntity<>(fileSubmissions, HttpStatus.OK);
+            List<FileSubmissionDTO> fileSubmissionDTOs = new ArrayList<>();
+            for (FileSubmission filesubmission : fileSubmissions) {
+                FileSubmissionDTO fileSubmissionDTO = new FileSubmissionDTO();
+                fileSubmissionDTO.setAssessmentId(filesubmission.getAssessmentId());
+                fileSubmissionDTO.setAssessmentTitle(filesubmission.getTitle());
+                fileSubmissionDTO.setAssessmentDescription(filesubmission.getDescription());
+                fileSubmissionDTO.setAssessmentMaxScore(filesubmission.getMaxScore());
+
+                DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                fileSubmissionDTO.setAssessmentStartDate(formatter.format(filesubmission.getStartDate()));
+                fileSubmissionDTO.setAssessmentEndDate(formatter.format(filesubmission.getEndDate()));
+
+                fileSubmissionDTOs.add(fileSubmissionDTO);
+            }
+            return new ResponseEntity<>(fileSubmissionDTOs, HttpStatus.OK);
 
         } catch (CourseNotFoundException ex) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -79,12 +133,12 @@ public class AssessmentController {
         }
     }
 
-    @DeleteMapping("/deleteFileSubmissionById/{fileSubmissionId}")
-    public ResponseEntity<HttpStatus> deleteFileSubmissionById(@PathVariable("fileSubmissionId") Long fileSubmissionId) {
+    @DeleteMapping("/deleteAssessmentById/{assessmentId}")
+    public ResponseEntity<HttpStatus> deleteAssessmentById(@PathVariable("assessmentId") Long assessmentId) {
         try {
-            fileSubmissionService.deleteFileSubmission(fileSubmissionId);
+            assessmentService.deleteAssessment(assessmentId);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (FileSubmissionNotFoundException ex) {
+        } catch (AssessmentNotFoundException ex) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
