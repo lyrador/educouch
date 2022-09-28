f119cb7dfcf03acde1dd7f60bfd424f2463fd37f
irene <irenelie1412@gmail.com>
2022-09-28 18:23:20 +0800

    data loading + classrun + calendar

---
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/model/ClassRun.java
@@ -0,0 +1,105 @@
+package com.educouch.educouchsystem.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ClassRun {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long classRunId;
+
+    @Column(nullable = false)
+    private LocalDate classRunStart;
+
+    @Column(nullable = false)
+    private LocalDate classRunEnd;
+
+    @OneToOne
+    @JoinColumn(name = "calendar")
+    private Calendar calendar;
+
+    @ManyToOne
+    private Instructor instructor;
+
+    @ManyToOne
+    private Course course;
+
+    @ManyToMany(mappedBy = "classRuns")
+    private List<Learner> enrolledLearners;
+
+    public ClassRun() {
+        this.enrolledLearners = new ArrayList<>();
+    }
+
+    public ClassRun(LocalDate classRunStart, LocalDate classRunEnd) {
+        new ClassRun();
+        this.classRunStart = classRunStart;
+        this.classRunEnd = classRunEnd;
+    }
+
+    public ClassRun(LocalDate c, LocalDate e, Instructor instructor, Calendar calendar) {
+        new ClassRun(c, e);
+        this.instructor = instructor;
+        this.calendar = calendar;
+    }
+
+    public Long getClassRunId() {
+        return classRunId;
+    }
+
+    public void setClassRunId(Long classRunId) {
+        this.classRunId = classRunId;
+    }
+
+    public LocalDate getClassRunStart() {
+        return classRunStart;
+    }
+
+    public void setClassRunStart(LocalDate classRunStart) {
+        this.classRunStart = classRunStart;
+    }
+
+    public LocalDate getClassRunEnd() {
+        return classRunEnd;
+    }
+
+    public void setClassRunEnd(LocalDate classRunEnd) {
+        this.classRunEnd = classRunEnd;
+    }
+
+    public List<Learner> getEnrolledLearners() {
+        return enrolledLearners;
+    }
+
+    public void setEnrolledLearners(List<Learner> enrolledLearners) {
+        this.enrolledLearners = enrolledLearners;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
