05fa361db62ff60b9c6fb591985e11330dc89318
lyrador <yeedaryl700@gmail.com>
2022-09-30 22:58:53 +0800

    adding classrun implementation

--- educouchsystem/src/main/java/com/educouch/educouchsystem/repository/LearnerRepository.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/repository/LearnerRepository.java
@@ -2,6 +2,7 @@ package com.educouch.educouchsystem.repository;
 
 import com.educouch.educouchsystem.model.Learner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
