af654a513d5a6bc2e103cef58d86a2eea40a129b
SrinivasG99 <78495960+SrinivasG99@users.noreply.github.com>
2022-10-01 17:41:18 +0800

    added attributes to organisation and updated transaction model to only service lms to org

--- educouchsystem/src/main/java/com/educouch/educouchsystem/repository/TransactionRepository.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/repository/TransactionRepository.java
@@ -7,12 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
-    @Query("SELECT t FROM Transaction t WHERE t.paymentType = 0")
-    public List<Transaction> findAllLearnerToLms();
-
-    @Query("SELECT t FROM Transaction t WHERE t.paymentType = 1")
-    public List<Transaction> findAllLmsToOrg();
-
-    @Query("SELECT t FROM Transaction t WHERE t.paymentType = 2")
-    public List<Transaction> findAllLmsToLearner();
+    public List<Transaction> findAll();
 }
