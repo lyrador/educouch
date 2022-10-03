36e0eab2d6701e2cdbb113e84f2a144459d338ed
SrinivasG99 <78495960+SrinivasG99@users.noreply.github.com>
2022-10-03 23:17:56 +0800

    backend to support edit and delete transactions

--- educouchsystem/src/main/java/com/educouch/educouchsystem/controller/TransactionController.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/controller/TransactionController.java
@@ -2,10 +2,12 @@ package com.educouch.educouchsystem.controller;
 
 import com.educouch.educouchsystem.model.Transaction;
 import com.educouch.educouchsystem.service.TransactionService;
+import com.educouch.educouchsystem.util.exception.TransactionNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -30,6 +32,27 @@ public class TransactionController {
     @GetMapping("/getAllLmsToOrgTransaction")
     public ResponseEntity<List<Transaction>> getAllLearnerToLms() {
         return ResponseEntity.status(HttpStatus.OK).body(transactionService.findAllTransactions());
+    }
+
+    @PostMapping("/updateTransaction")
+    public ResponseEntity<Transaction> updateTransaction(@RequestBody Transaction transaction)  {
+        Transaction transaction1 = null;
+        try {
+            transaction1 = transactionService.updateTransaction(transaction);
+        } catch (TransactionNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find Transaction", e);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(transaction1);
+    }
+
+    @PostMapping("/deleteTransaction")
+    public ResponseEntity<Long> deleteTransaction(@RequestBody Transaction transaction)  {
+        try {
+    transactionService.deleteTransaction(transaction);
+        } catch (TransactionNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find Transaction", e);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(transaction.getTransactionId());
     }
 
 
