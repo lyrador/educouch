bd5e6c141c73f12f8f04cfd5967b34f85a27c8e5
irene <irenelie1412@gmail.com>
2022-10-14 14:21:23 +0800

    whiteboard

---
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/dto/ActionResponseDTO.java
@@ -0,0 +1,29 @@
+package com.educouch.educouchsystem.dto;
+
+import java.util.List;
+
+public class ActionResponseDTO {
+    private String message;
+    private List<String> users;
+
+    public ActionResponseDTO(String message, List<String> users) {
+        this.message = message;
+        this.users = users;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<String> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<String> users) {
+        this.users = users;
+    }
+}
