ffcf358b5d444d88382cfd0abbbffeb8ff533664
KaiYu <kaiyu.chong@gmail.com>
2022-10-17 22:26:41 +0800

    quiz attempt controller half done

--- educouchsystem/src/main/java/com/educouch/educouchsystem/repository/QuizAttemptRepository.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/repository/QuizAttemptRepository.java
@@ -2,8 +2,13 @@ package com.educouch.educouchsystem.repository;
 
 import com.educouch.educouchsystem.model.QuizAttempt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuizAttemptRepository extends JpaRepository<QuizAttempt, Long> {
+    @Query("SELECT q FROM QuizAttempt q WHERE q.learner.learnerId=:learnerId")
+    List<QuizAttempt> findQuizAttemptsByLearnerId(Long learnerId);
 }
