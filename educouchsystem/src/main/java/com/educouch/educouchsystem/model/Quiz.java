fed132a06e81930a3cbb82725937f4d25046000e
KaiYu <kaiyu.chong@gmail.com>
2022-10-17 22:26:41 +0800

    quiz attempt controller half done

--- educouchsystem/src/main/java/com/educouch/educouchsystem/model/Quiz.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/model/Quiz.java
@@ -30,14 +30,9 @@ public class Quiz extends Assessment implements Serializable {
     @JoinColumn(name="quiz_id")
     private List<Question> quizQuestions;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-    @JoinColumn(name="quiz_id")
-    private List<QuizAttempt> quizAttempts;
-
     public Quiz() {
         super();
         this.quizQuestions = new ArrayList<>();
-        this.quizAttempts = new ArrayList<>();
     }
 
     public Quiz(String title, String description, Double maxScore, Date startDate, Date endDate, Boolean hasTimeLimit, Integer timeLimit, Boolean isAutoRelease) {
@@ -45,7 +40,6 @@ public class Quiz extends Assessment implements Serializable {
         this.hasTimeLimit = hasTimeLimit;
         this.isAutoRelease = isAutoRelease;
         this.quizQuestions = new ArrayList<>();
-        this.quizAttempts = new ArrayList<>();
     }
 
     public Boolean getHasTimeLimit() {
@@ -72,13 +66,13 @@ public class Quiz extends Assessment implements Serializable {
         this.quizQuestions = quizQuestions;
     }
 
-    public List<QuizAttempt> getQuizAttempts() {
-        return quizAttempts;
-    }
-
-    public void setQuizAttempts(List<QuizAttempt> quizAttempts) {
-        this.quizAttempts = quizAttempts;
-    }
+//    public List<QuizAttempt> getQuizAttempts() {
+//        return quizAttempts;
+//    }
+//
+//    public void setQuizAttempts(List<QuizAttempt> quizAttempts) {
+//        this.quizAttempts = quizAttempts;
+//    }
 
     public Integer getTimeLimit() {
         return timeLimit;
--- educouchsystem/src/main/java/com/educouch/educouchsystem/repository/QuestionAttemptRepository.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/repository/QuestionAttemptRepository.java
@@ -1,9 +1,19 @@
 package com.educouch.educouchsystem.repository;
 
+import com.educouch.educouchsystem.model.Instructor;
 import com.educouch.educouchsystem.model.QuestionAttempt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuestionAttemptRepository extends JpaRepository<QuestionAttempt, Long> {
+
+//    @Query("SELECT i FROM Instructor i WHERE i.username=:username")
+//    Instructor findInstructorByUsername(String username);
+
+//    List<QuestionAttempt> findQuestionAttemptsByLearnerId
+
 }
