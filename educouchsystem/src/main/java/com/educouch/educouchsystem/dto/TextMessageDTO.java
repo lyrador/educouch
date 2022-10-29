a340f19967d1e3be8ecf078394ac3ad8dbcedc2d
irene <irenelie1412@gmail.com>
2022-10-29 19:11:43 +0800

    added text message

--- educouchsystem/src/main/java/com/educouch/educouchsystem/dto/TextMessageDTO.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/dto/TextMessageDTO.java
@@ -3,6 +3,10 @@ package com.educouch.educouchsystem.dto;
 public class TextMessageDTO {
 
     private String message;
+    private String author;
+
+    public TextMessageDTO() {
+    }
 
     public TextMessageDTO(String message) {
         this.message = message;
@@ -16,4 +20,11 @@ public class TextMessageDTO {
         this.message = message;
     }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
