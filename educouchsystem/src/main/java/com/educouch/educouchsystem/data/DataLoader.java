82e69a2e772524fcb8487dee45e89a46fcee3626
irene <irenelie1412@gmail.com>
2022-11-14 21:39:27 +0800

    fix

--- educouchsystem/src/main/java/com/educouch/educouchsystem/data/DataLoader.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/data/DataLoader.java
@@ -114,7 +114,7 @@ public class DataLoader implements CommandLineRunner {
         Learner learner_4 = learnerRepository.save(new Learner("David", "lielieirene@gmail.com", "password",
                 "david",
                 "https://educouchbucket.s3.ap-southeast-1.amazonaws.com/1665376079555_png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png",
-                false, "23456"));
+                true, "23456"));
 
         // create organisation and organisation admin
         Organisation org1 = new Organisation("FakeTuition", "issa fake tuition", "23423423234234");
@@ -180,6 +180,187 @@ public class DataLoader implements CommandLineRunner {
             classRunOne = listOfClassRuns.get(0);
             try {
                 stripeService.payDeposit(classRunOne.getClassRunId(), learner_1.getLearnerId(), new BigDecimal(100));
+                // stripeService.payCourseFee(classRunOne.getClassRunId(),
+                // learner_1.getLearnerId(), new BigDecimal(900));
+            } catch (ClassRunNotFoundException | LearnerNotFoundException ex) {
+                System.out.println("Error in enrolment.");
+            }
+        } catch (CourseNotFoundException ex) {
+            System.out.println("Error in generating class run. ");
+        }
+
+
+        // create a course and link it to head instructor horlicks of organisation org 1
+        Course cs2102k = new Course();
+        cs2102k.setCourseCode("CS2102K");
+        cs2102k.setCourseTitle("Database Systems for Kids");
+        cs2102k.setCourseDescription("This is a new CS2102 course!");
+        cs2102k.setCourseTimeline("TILL END 2022");
+        cs2102k.setCourseMaxScore(100.00);
+        cs2102k.setAgeGroup(AgeGroupEnum.KIDS);
+        cs2102k.setCourseFee(new BigDecimal(1000));
+        cs2102k.setStartDate(LocalDate.now().plusDays(7));
+        // set it to live status to test enrollment functionality (in normal situations
+        // classruns should be created first before making the course live)
+        cs2102k.setCourseApprovalStatus(CourseApprovalStatusEnum.LIVE);
+
+        if (i2.getCourses() == null) {
+            List<Course> courseList = new ArrayList<>();
+            i2.setCourses(courseList);
+        }
+        i2.getCourses().add(cs2102k);
+
+        if (org1.getCourses() == null) {
+            List<Course> courseList = new ArrayList<>();
+            org1.setCourses(courseList);
+        }
+        org1.getCourses().add(cs2102k);
+        cs2102k.setOrganisation(org1);
+
+        if (cs2102k.getInstructors() == null) {
+            List<Instructor> instructorList = new ArrayList<>();
+            cs2102k.setInstructors(instructorList);
+        }
+        cs2102k.getInstructors().add(i2);
+
+        cs2102k = courseService.saveCourse(cs2102k);
+
+        // from here can create classruns and enrol learners in the course cs2102 to
+        // test functionality
+        Integer[] daysOfWeek1 = new Integer[2];
+        daysOfWeek1[0] = 1;
+        daysOfWeek1[1] = 2;
+        ClassRun classRunOne1 = new ClassRun(LocalDate.now().plusDays(7), LocalDate.now().plusDays(90),
+                LocalTime.MIDNIGHT, LocalTime.NOON, 1, 10, daysOfWeek, RecurringEnum.ALTERNATE);
+        classRunOne1.setInstructor(i2);
+        try {
+            courseService.addClassRunToCourse(cs2102k.getCourseId(), classRunOne1);
+            cs2102k = courseService.getCourseById(cs2102k.getCourseId());
+            int size = cs2102k.getClassRuns().size();
+            List<ClassRun> listOfClassRuns = cs2102k.getClassRuns();
+            classRunOne1 = listOfClassRuns.get(0);
+            try {
+                stripeService.payDeposit(classRunOne1.getClassRunId(), learner_4.getLearnerId(), new BigDecimal(100));
+                // stripeService.payCourseFee(classRunOne.getClassRunId(),
+                // learner_1.getLearnerId(), new BigDecimal(900));
+            } catch (ClassRunNotFoundException | LearnerNotFoundException ex) {
+                System.out.println("Error in enrolment.");
+            }
+        } catch (CourseNotFoundException ex) {
+            System.out.println("Error in generating class run. ");
+        }
+
+        // create a course and link it to head instructor horlicks of organisation org 1
+        Course cs2105 = new Course();
+        cs2105.setCourseCode("CS2105");
+        cs2105.setCourseTitle("Introduction to Computer Network");
+        cs2105.setCourseDescription("This is a new CS2102 course!");
+        cs2105.setCourseTimeline("TILL END 2022");
+        cs2105.setCourseMaxScore(100.00);
+        cs2105.setAgeGroup(AgeGroupEnum.ADULTS);
+        cs2105.setCourseFee(new BigDecimal(1000));
+        cs2105.setStartDate(LocalDate.now().plusDays(7));
+        // set it to live status to test enrollment functionality (in normal situations
+        // classruns should be created first before making the course live)
+        cs2105.setCourseApprovalStatus(CourseApprovalStatusEnum.LIVE);
+
+        if (i2.getCourses() == null) {
+            List<Course> courseList = new ArrayList<>();
+            i2.setCourses(courseList);
+        }
+        i2.getCourses().add(cs2105);
+
+        if (org1.getCourses() == null) {
+            List<Course> courseList = new ArrayList<>();
+            org1.setCourses(courseList);
+        }
+        org1.getCourses().add(cs2105);
+        cs2105.setOrganisation(org1);
+
+        if (cs2105.getInstructors() == null) {
+            List<Instructor> instructorList = new ArrayList<>();
+            cs2105.setInstructors(instructorList);
+        }
+        cs2105.getInstructors().add(i2);
+
+        cs2105 = courseService.saveCourse(cs2105);
+
+        // from here can create classruns and enrol learners in the course cs2102 to
+        // test functionality
+        Integer[] daysOfWeek2 = new Integer[2];
+        daysOfWeek2[0] = 1;
+        daysOfWeek2[1] = 2;
+        ClassRun classRunOne2 = new ClassRun(LocalDate.now().plusDays(7), LocalDate.now().plusDays(90),
+                LocalTime.MIDNIGHT, LocalTime.NOON, 1, 10, daysOfWeek, RecurringEnum.ALTERNATE);
+        classRunOne2.setInstructor(i2);
+        try {
+            courseService.addClassRunToCourse(cs2105.getCourseId(), classRunOne2);
+            cs2105 = courseService.getCourseById(cs2105.getCourseId());
+            int size = cs2105.getClassRuns().size();
+            List<ClassRun> listOfClassRuns = cs2105.getClassRuns();
+            classRunOne2 = listOfClassRuns.get(0);
+            try {
+                stripeService.payDeposit(classRunOne2.getClassRunId(), learner_1.getLearnerId(), new BigDecimal(100));
+                // stripeService.payCourseFee(classRunOne.getClassRunId(),
+                // learner_1.getLearnerId(), new BigDecimal(900));
+            } catch (ClassRunNotFoundException | LearnerNotFoundException ex) {
+                System.out.println("Error in enrolment.");
+            }
+        } catch (CourseNotFoundException ex) {
+            System.out.println("Error in generating class run. ");
+        }
+
+        // create a course and link it to head instructor horlicks of organisation org 1
+        Course cs2105k = new Course();
+        cs2105k.setCourseCode("CS2105K");
+        cs2105k.setCourseTitle("Introduction to Computer Network for Kids");
+        cs2105k.setCourseDescription("This is a new CS2102 course!");
+        cs2105k.setCourseTimeline("TILL END 2022");
+        cs2105k.setCourseMaxScore(100.00);
+        cs2105k.setAgeGroup(AgeGroupEnum.KIDS);
+        cs2105k.setCourseFee(new BigDecimal(1000));
+        cs2105k.setStartDate(LocalDate.now().plusDays(7));
+        // set it to live status to test enrollment functionality (in normal situations
+        // classruns should be created first before making the course live)
+        cs2105k.setCourseApprovalStatus(CourseApprovalStatusEnum.LIVE);
+
+        if (i2.getCourses() == null) {
+            List<Course> courseList = new ArrayList<>();
+            i2.setCourses(courseList);
+        }
+        i2.getCourses().add(cs2105k);
+
+        if (org1.getCourses() == null) {
+            List<Course> courseList = new ArrayList<>();
+            org1.setCourses(courseList);
+        }
+        org1.getCourses().add(cs2105k);
+        cs2105k.setOrganisation(org1);
+
+        if (cs2105k.getInstructors() == null) {
+            List<Instructor> instructorList = new ArrayList<>();
+            cs2105k.setInstructors(instructorList);
+        }
+        cs2105k.getInstructors().add(i2);
+
+        cs2105k = courseService.saveCourse(cs2105k);
+
+        // from here can create classruns and enrol learners in the course cs2102 to
+        // test functionality
+        Integer[] daysOfWeek3 = new Integer[2];
+        daysOfWeek3[0] = 1;
+        daysOfWeek3[1] = 2;
+        ClassRun classRunOne3 = new ClassRun(LocalDate.now().plusDays(7), LocalDate.now().plusDays(90),
+                LocalTime.MIDNIGHT, LocalTime.NOON, 1, 10, daysOfWeek, RecurringEnum.ALTERNATE);
+        classRunOne3.setInstructor(i2);
+        try {
+            courseService.addClassRunToCourse(cs2105k.getCourseId(), classRunOne3);
+            cs2105k = courseService.getCourseById(cs2105k.getCourseId());
+            int size = cs2105k.getClassRuns().size();
+            List<ClassRun> listOfClassRuns = cs2105k.getClassRuns();
+            classRunOne3 = listOfClassRuns.get(0);
+            try {
+                stripeService.payDeposit(classRunOne3.getClassRunId(), learner_4.getLearnerId(), new BigDecimal(100));
                 // stripeService.payCourseFee(classRunOne.getClassRunId(),
                 // learner_1.getLearnerId(), new BigDecimal(900));
             } catch (ClassRunNotFoundException | LearnerNotFoundException ex) {
