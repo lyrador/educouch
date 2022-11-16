b13bdf968d9dfb8493d5f9d41e24f50cbcd6f363
SrinivasG99 <78495960+SrinivasG99@users.noreply.github.com>
2022-11-17 04:33:55 +0800

    points for assessment creation

---
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/util/exception/PointsWalletNotFoundException.java
@@ -0,0 +1,11 @@
+package com.educouch.educouchsystem.util.exception;
+
+public class PointsWalletNotFoundException extends Exception {
+
+    public PointsWalletNotFoundException() {
+    }
+
+    public PointsWalletNotFoundException(String msg) {
+        super(msg);
+    }
+}
