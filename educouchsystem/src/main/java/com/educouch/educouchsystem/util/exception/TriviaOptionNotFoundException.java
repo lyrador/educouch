f0fd458388a3baf1b4634e8538d58271ad846d5f
sachok123 <e0540367@u.nus.edu>
2022-11-16 15:55:40 +0800

    backend done up for Trivia Portion of In class participation module

---
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/util/exception/TriviaOptionNotFoundException.java
@@ -0,0 +1,10 @@
+package com.educouch.educouchsystem.util.exception;
+
+public class TriviaOptionNotFoundException extends Exception{
+    public TriviaOptionNotFoundException() {
+    }
+
+    public TriviaOptionNotFoundException(String message) {
+        super(message);
+    }
+}
