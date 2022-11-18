8b9ad4034427a256737adb2a6e68074001e2584c
sachok123 <e0540367@u.nus.edu>
2022-11-18 14:22:37 +0800

    poll in progress

--- educouchsystem/src/main/java/com/educouch/educouchsystem/model/Poll.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/model/Poll.java
@@ -1,5 +1,7 @@
 package com.educouch.educouchsystem.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
@@ -82,6 +84,7 @@ public class Poll {
         this.numOfQuestions = numOfQuestions;
     }
 
+    @JsonIgnore
     public ClassRun getClassRun() {
         return classRun;
     }
