b43dc1130293d1fefa3f32fc58d04774d7130df9
irene <irenelie1412@gmail.com>
2022-11-19 03:13:59 +0800

    can pay with discounts

--- educouchsystem/src/main/java/com/educouch/educouchsystem/service/PointsWalletServiceImpl.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/service/PointsWalletServiceImpl.java
@@ -1,17 +1,33 @@
 package com.educouch.educouchsystem.service;
 
+import com.educouch.educouchsystem.model.Course;
+import com.educouch.educouchsystem.model.Organisation;
 import com.educouch.educouchsystem.model.PointsWallet;
 import com.educouch.educouchsystem.repository.PointsWalletRepository;
+import com.educouch.educouchsystem.util.exception.CourseNotFoundException;
 import com.educouch.educouchsystem.util.exception.PointsWalletNotFoundException;
+import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PointsWalletServiceImpl implements PointsWalletService{
     @Autowired
     PointsWalletRepository pointsWalletRepository;
+
+    @Autowired
+    CourseService courseService;
+
+    @Autowired
+    OrganisationService organisationService;
+
+    @Autowired
+    LearnerService learnerService;
 
     @Override
     public PointsWallet createWallet(Long learnerId, Long organisationId, String orgName) {
@@ -48,5 +64,73 @@ public class PointsWalletServiceImpl implements PointsWalletService{
             throw new PointsWalletNotFoundException("Wallet does not exist!");
         }
         return wallet;
+    }
+
+    @Override
+    public Long findLearnerPoints(Long learnerId, Long courseId) throws PointsWalletNotFoundException{
+        try {
+            Course course = courseService.getCourseById(courseId);
+            Organisation org = course.getOrganisation();
+            PointsWallet pw = pointsWalletRepository.findByLearnerIdOrganisationId(learnerId, org.getOrganisationId());
+            if(pw != null) {
+                return pw.getDiscountPoints();
+            } else {
+                throw new PointsWalletNotFoundException("Unable to find any wallet!");
+            }
+
+        } catch(CourseNotFoundException ex) {
+            return new Long(0);
+        }
+    }
+
+    @Override
+    public Double retrievePriceDiscount(Long learnerId, Long courseId) throws CourseNotFoundException, PointsWalletNotFoundException {
+        // discount can only be implemented when they ae paying for 'remaining course fee' NOT 'deposit'
+        try {
+            Course course = courseService.getCourseById(courseId);
+            Organisation org = course.getOrganisation();
+            PointsWallet pw = pointsWalletRepository.findByLearnerIdOrganisationId(learnerId, org.getOrganisationId());
+            if(pw != null) {
+                Double currency = org.getCurrencyConversionNumber();
+                Double points = org.getRewardPointsConversionNumber();
+                Integer ownedPoints = pw.getDiscountPoints().intValue();
+
+                Double maxDiscount =  Math.floor(ownedPoints / points) * currency;
+                Double neededDiscount = course.getCourseFee().doubleValue() * 0.90;
+
+                if(maxDiscount < neededDiscount) {
+                    return maxDiscount;
+                } else {
+                    return neededDiscount;
+                }
+            } else {
+                throw new PointsWalletNotFoundException("Unable to find any wallet!");
+            }
+
+        } catch(CourseNotFoundException ex) {
+            return 0.0;
+        }
+    }
+
+    @Override
+    public Map<String, Double> retrieveCourseConversionRate(Long courseId) throws CourseNotFoundException {
+        Map<String, Double> conversionRate = new HashMap<String, Double>();
+        try {
+            Course course = courseService.getCourseById(courseId);
+            Organisation org = course.getOrganisation();
+            Double currency = org.getCurrencyConversionNumber();
+            Double points = org.getRewardPointsConversionNumber();
+
+            conversionRate.put("points", points);
+            conversionRate.put("currency", currency);
+
+            return conversionRate;
+
+        } catch(CourseNotFoundException ex) {
+            conversionRate.put("points",1.0);
+            conversionRate.put("currency", 0.0);
+            return conversionRate;
+
+        }
     }
 }
