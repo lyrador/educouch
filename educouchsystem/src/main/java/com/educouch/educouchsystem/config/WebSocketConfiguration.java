8952f2e9671a7633a9c82562d9b749177968b9d9
irene <irenelie1412@gmail.com>
2022-11-23 00:39:34 +0800

    Revert "enhancement works"
    This reverts commit 2a2f4e516f8b8d1d020f4b73711228bef08a7258.

--- educouchsystem/src/main/java/com/educouch/educouchsystem/config/WebSocketConfiguration.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/config/WebSocketConfiguration.java
@@ -2,21 +2,15 @@ package com.educouch.educouchsystem.config;
 
 import com.educouch.educouchsystem.component.SocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfiguration  implements WebSocketMessageBrokerConfigurer {
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // with sockjs
-        registry.addEndpoint("/videochat").setAllowedOrigins("http://localhost:3000").withSockJS();
-        // without sockjs
-        //registry.addEndpoint("/ws-message").setAllowedOriginPatterns("*");
+public class WebSocketConfiguration  implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/videochat").setAllowedOrigins("*");
     }
 }
