f43441bb56938b1f93024831ffe0cddb2eb092a5
irene <irenelie1412@gmail.com>
2022-11-22 22:46:39 +0800

    enhancement works

--- educouchsystem/src/main/java/com/educouch/educouchsystem/config/WebSocketConfiguration.java
+++ educouchsystem/src/main/java/com/educouch/educouchsystem/config/WebSocketConfiguration.java
@@ -2,15 +2,21 @@ package com.educouch.educouchsystem.config;
 
 import com.educouch.educouchsystem.component.SocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfiguration  implements WebSocketConfigurer {
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SocketHandler(), "/videochat").setAllowedOrigins("*");
+public class WebSocketConfiguration  implements WebSocketMessageBrokerConfigurer {
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // with sockjs
+        registry.addEndpoint("/videochat").setAllowedOrigins("http://localhost:3000").withSockJS();
+        // without sockjs
+        //registry.addEndpoint("/ws-message").setAllowedOriginPatterns("*");
     }
 }
